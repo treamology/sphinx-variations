8d55e0686439350542c533d54e67d3a048711626
Donny Lawrence <trea@treamous.com>
2019-05-13 19:17:32 -0500

    Initial commit.

---
+++ variations.py
@@ -0,0 +1,96 @@
+import os
+
+from sphinx.util import status_iterator
+from sphinx.util.osutil import os_path
+import sphinx.builders.html
+import sphinx.directives.other
+import sphinx.addnodes
+import sphinx.util.build_phase
+
+import docutils.nodes
+import docutils.io
+
+class VariationNode(docutils.nodes.Element):
+    """ Node that is used to differentate from a regular `only` node. """
+
+
+class OnlyVariationDirective(sphinx.directives.other.Only):
+    """
+    A simple directive that overrides the directive for `only`, but basically
+    retains the same functionality.
+    """
+
+    def run(self):
+        """
+        Checks if the tag is in the list of doc variations. If it is, turn it
+        into a `VariationNode`
+        """
+        if self.arguments[0] in self.config.variations:
+            nodes = super().run()
+            if nodes:
+                nodes[0].__class__ = VariationNode
+            return nodes
+
+        return super().run()
+
+
+class HTMLVariationBuilder(sphinx.builders.html.StandaloneHTMLBuilder):
+    """
+    Outputs multiple variations of the documentation, with differing variations
+    enabled/disabled.
+    """
+
+    def get_outfilename(self, pagename):
+        """
+        Same as super()'s, but adds in the current variation as an
+        intermediate directory.
+        """
+        return os.path.join(self.outdir,
+                            self.docwriter.current_variation,
+                            os_path(pagename) + self.out_suffix)
+
+    def get_target_uri(self, docname, typ=None):
+        """
+        Same as super()'s, but adds in the current variation as an
+        intermediate directory.
+        """
+        return os.path.join(self.docwriter.current_variation,
+                            docname + self.link_suffix)
+
+    def _write_serial(self, docnames):
+        """
+        Loops through each variation, which causes each docname to be
+        written multiple times depending on which is active.
+        """
+        for docname in status_iterator(docnames, 'writing output... ', "darkgreen",
+                                       len(docnames), self.app.verbosity):
+            for variation in self.config.variations:
+                self.docwriter.current_variation = variation
+
+                self.app.phase = sphinx.util.build_phase.BuildPhase.RESOLVING
+                doctree = self.env.get_and_resolve_doctree(docname, self)
+                self.app.phase = sphinx.util.build_phase.BuildPhase.WRITING
+                self.write_doc_serialized(docname, doctree)
+                self.write_doc(docname, doctree)
+
+
+def visit_variation_node(self, node):
+    """ Skip the node if it's not the current variation, otherwise noop. """
+    if self.builder.docwriter.current_variation != node['expr']:
+        raise docutils.nodes.SkipNode
+
+def depart_variation_node(self, node):
+    pass
+
+def setup(app):
+    app.add_config_value('variations', [], 'env')
+
+    app.add_builder(HTMLVariationBuilder, override=True)
+    app.add_node(VariationNode, html=(visit_variation_node, depart_variation_node))
+    app.add_directive('only', OnlyVariationDirective, override=True)
+
+    return {
+        'version': '0.1',
+        'parallel_read_safe': True,
+        'parallel_write_safe': False,
+    }
